dd971695080481cda2415965023aafc19f20e4c3
joao127 <65619577+joao127@users.noreply.github.com>
2020-05-19 15:28:25 -0300

    Add files via upload

---
+++ exempo.py
@@ -0,0 +1,25 @@
+nome = joao 
+idade = 20
+ altura= 1.76
+ solteiro = true
+
+ a = 2
+ b = 3
+
+ divisao a / b
+
+ a = 2
+ b = 3
+
+ soma a + b
+
+ a = 2
+ b = 3
+
+ multiplicaçao a * b
+
+ a = int(intup())
+ b = int(intup())
+
+ soma = a + b
+ print (soma)
